e24334ca3589fe17ff80765458888ffbb259efb9
Nicolas-CARRERE <nicolascarrere.pro@gmail.com>
2023-01-11 22:48:41 +0100

    install taillwind / add Layout / add navbar

---
+++ src/components/organisms/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+function Footer({}: Props) {
+  return (
+    <div className="w-screen h-[10%] flex p-5 text-xl bg-slate-50">Footer</div>
+  );
+}
+
+export default Footer;

406aedf6694c8a567c2b045c2f7cd4b711dbdf77
Pupuce-64 <laeti.decandt@gmail.com>
2023-01-12 13:39:19 +0100

    added signup page with user creation and redirection on signin page

---
+++ pages/signup/index.tsx
@@ -0,0 +1,112 @@
+import { User } from "@prisma/client";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+function Signup() {
+  const [newUser, setNewUser] = useState<Partial<User>>({
+    firstname: "",
+    lastname: "",
+    email: "",
+    birthday: "",
+    adresse: "",
+    BP: "",
+    noPermis: "",
+    password: "",
+  });
+  const router = useRouter();
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewUser({ ...newUser, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = () => {
+    axios.post("/api/auth/signup", newUser);
+    router.push("/signin");
+  };
+
+  return (
+    <div>
+      <form className="flex flex-col w-full m-auto align-middle h-screen mt-10">
+        <h1 className="from-neutral-500 font-serif ml-12 mb-5">
+          Vos informations personnelles
+        </h1>
+        <input
+          onChange={(e) => handleChange(e)}
+          value={newUser.firstname}
+          className="form"
+          type="text"
+          name="firstname"
+          placeholder="Firstname"
+        />
+        <input
+          onChange={(e) => handleChange(e)}
+          className="form"
+          type="text"
+          name="lastname"
+          placeholder="Lastname"
+        />
+        <input
+          onChange={(e) => setNewUser({ ...newUser, birthday: e.target.value })}
+          className="form"
+          type="date"
+          name="birthday"
+        />
+        <input
+          onChange={(e) => handleChange(e)}
+          className="form"
+          type="text"
+          name="adresse"
+          placeholder="Address"
+        />
+        <input
+          onChange={(e) => handleChange(e)}
+          className="form"
+          type="text"
+          name="BP"
+          placeholder="Postal code"
+        />
+        <input
+          onChange={(e) => handleChange(e)}
+          className="form"
+          type="text"
+          name="town"
+          placeholder="Town"
+        />
+        <input
+          onChange={(e) => handleChange(e)}
+          className="form"
+          type="text"
+          name="noPermis"
+          placeholder="Driver license number"
+        />
+        <h1 className="from-neutral-500 font-serif ml-12 mt-5 mb-5">
+          Vos identifiants
+        </h1>
+        <input
+          onChange={(e) => handleChange(e)}
+          className="form"
+          type="email"
+          name="email"
+          placeholder="Email"
+        />
+        <input
+          onChange={(e) => handleChange(e)}
+          className="form"
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
+
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="border border-gray-500 flex w-40 h-8 m-auto my-2   bg-gradient-to-r from-[#43BF9C] via-green-300 to-[#43BF9C] rounded-md justify-center"
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
--- src/components/organisms/NavBarUser.tsx
+++ src/components/organisms/NavBarUser.tsx
@@ -11,9 +11,7 @@ function NavBarUser({}: Props) {
   const [isSideBarVisible, setIsSidebarVisible] = useState(false);
 
   return (
-
-    <div className="w-screen h-[10%] fixed top-0 flex items-center justify-between p-5 text-xl bg-[#43BF9C]">
-
+    <div className="w-screen h-[10%] sticky top-0 flex items-center justify-between p-5 text-xl bg-[#43BF9C]">
       {isSideBarVisible ? (
         <Image
           className="flex text-4xl text-black items-center cursor-pointer z-50"
@@ -36,31 +34,36 @@ function NavBarUser({}: Props) {
         className={`ease-in-out duration-300
          top-[10%] left-0 w-[40vw] bg-[#43BF9C]  p-10 pl-20 text-black fixed h-full z-40 ${
            isSideBarVisible ? "translate-x-0 " : "translate-x-[-100%]"
-         }`}>
+         }`}
+      >
         {isSideBarVisible && (
           <div className="bg-[#43BF9C] w-[40vw] h-[100%] fixed top-[10%] left-0 z-10 flex flex-col duration-300">
             <Link
               className="px-[1em] py-[1em]"
               onClick={() => setIsSidebarVisible(!isSideBarVisible)}
-              href="/">
+              href="/"
+            >
               Home
             </Link>
             <Link
               className="px-[1em] py-[1em]"
               onClick={() => setIsSidebarVisible(!isSideBarVisible)}
-              href="/cars">
+              href="/cars"
+            >
               Cars
             </Link>
             <Link
               className="px-[1em] py-[1em]"
               onClick={() => setIsSidebarVisible(!isSideBarVisible)}
-              href="/cars/new-car">
+              href="/cars/new-car"
+            >
               New car
             </Link>
             <Link
               className="px-[1em] py-[1em]"
               onClick={() => setIsSidebarVisible(!isSideBarVisible)}
-              href="/availability">
+              href="/availability"
+            >
               Availability
             </Link>
           </div>

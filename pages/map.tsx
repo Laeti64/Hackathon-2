922d30bed9c37e9a6e255a099644f1981211b76e
Pupuce-64 <laeti.decandt@gmail.com>
2023-01-12 23:05:48 +0100

    added agencies POI

--- pages/map.tsx
+++ pages/map.tsx
@@ -3,31 +3,39 @@ import React, { useEffect, useState } from "react";
 import { Marker, Popup } from "react-map-gl";
 import { MapView, Heading, Text, LocationSearch } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
-
+import agencyFetcher from "../services/agenciesFetcher";
 import "@aws-amplify/ui-react/styles.css";
-
 import { Auth } from "aws-amplify";
-
 import awsExports from "../src/aws-exports";
-import axios from "axios";
-import axiosInstance from "../services/axiosInstance";
+import { TAgency } from "../src/types/types";
+import { useRouter } from "next/router";
 
 Amplify.configure(awsExports);
 
 function MarkerWithPopup({
   latitude,
   longitude,
+  name,
+  adresse,
+  id,
 }: {
   latitude: number;
   longitude: number;
+  name: String;
+  adresse: String;
+  id: String;
 }) {
   const [showPopup, setShowPopup] = useState(false);
-
+  const router = useRouter();
   const handleMarkerClick = ({ originalEvent }: any) => {
     originalEvent.stopPropagation();
     setShowPopup(true);
   };
 
+  const handleRoute = () => {
+    console.log(id);
+    router.push(`/agencies/${id}`);
+  };
   return (
     <>
       <Marker
@@ -42,8 +50,10 @@ function MarkerWithPopup({
           offset={{ bottom: [0, -40] }}
           onClose={() => setShowPopup(false)}
         >
-          <Heading level={2}>Marker Information</Heading>
-          <Text>Some information about a location.</Text>
+          <Heading onClick={handleRoute} level={5}>
+            {name}
+          </Heading>
+          <Text onClick={handleRoute}>{adresse}</Text>
         </Popup>
       )}
     </>
@@ -63,18 +73,29 @@ async function signIn() {
 
 export default function Map() {
   const [geo, setGeo] = useState<any>(null);
+  const [agencies, setAgencies] = useState<TAgency[]>([]);
 
   useEffect(() => {
     signIn();
     navigator.geolocation.getCurrentPosition((position) => {
       setGeo(position.coords);
-      console.log(position);
     });
   }, []);
-
+  console.log(geo);
+  useEffect(() => {
+    agencyFetcher.getAgencies().then((data) => {
+      if (data) {
+        setAgencies(data);
+      }
+    });
+  }, []);
+  console.log(agencies);
   return (
-    <div className="w-screen h-screen">
-      <div className="relative w-2/3 h-2/3 m-auto mt-10">
+    <div className="flex flex-col w-screen h-screen">
+      <h1 className="from-neutral-500 font-serif mx-auto mt-5">
+        Select your favourite agency
+      </h1>
+      <div className="relative w-2/3 h-1/2 m-auto mt-5">
         {geo && (
           <MapView
             style={{
@@ -85,12 +106,21 @@ export default function Map() {
             initialViewState={{
               latitude: geo.latitude,
               longitude: geo.longitude,
+              zoom: 9,
             }}
           >
-            <MarkerWithPopup
-              latitude={geo.latitude}
-              longitude={geo.longitude}
-            />
+            {agencies.map((e) => {
+              return (
+                <MarkerWithPopup
+                  key={e.id as string}
+                  latitude={+e.lat}
+                  longitude={+e.lng}
+                  name={e.name}
+                  adresse={e.address}
+                  id={e.id}
+                />
+              );
+            })}
 
             <LocationSearch />
           </MapView>
---
+++ services/agenciesFetcher.ts
@@ -0,0 +1,64 @@
+import { TAgency } from "../src/types/types";
+import axiosInstance from "./axiosInstance";
+
+const agencyFetcher = {
+  createAgency: async (data: string) => {
+    try {
+      await axiosInstance.post<TAgency>(
+        "/agencies",
+        JSON.parse(JSON.stringify(data))
+      );
+    } catch (error) {
+      throw error;
+    } finally {
+      console.log("Agency creation is ok !");
+    }
+  },
+
+  getAgencies: async () => {
+    try {
+      const { data } = await axiosInstance.get<TAgency[]>("/agencies");
+      return data ? data : null;
+    } catch (error) {
+      throw error;
+    } finally {
+      console.log("get Agencies is ok !");
+    }
+  },
+
+  getAgencyById: async (id: string) => {
+    try {
+      const { data } = await axiosInstance.get<TAgency>(`/agencies/${id}`);
+      return data ? data : null;
+    } catch (err) {
+      console.error(err);
+    } finally {
+      console.log("getAgencyById is ok !");
+    }
+  },
+
+  updateAgencyById: async (id: string, data: string) => {
+    try {
+      await axiosInstance.put<TAgency>(
+        `/agencies/${id}`,
+        JSON.parse(JSON.stringify(data))
+      );
+    } catch (err) {
+      console.error(err);
+    } finally {
+      console.log("updateAgencyById is ok !");
+    }
+  },
+
+  deleteAgencyById: async (id: string) => {
+    try {
+      await axiosInstance.delete<TAgency>(`/agencies/${id}`);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      console.log("deleteAgencyById is ok !");
+    }
+  },
+};
+
+export default agencyFetcher;

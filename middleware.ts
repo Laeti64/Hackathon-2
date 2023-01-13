47bf4d44421f5e70dd0ed59b8d7716627995c70c
Pupuce-64 <laeti.decandt@gmail.com>
2023-01-13 03:47:36 +0100

    fixed path middleware

--- middleware.ts
+++ middleware.ts
@@ -23,13 +23,5 @@ export async function middleware(request: NextRequest): Promise<NextResponse> {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: [
-    // "/api/users/:path*",
-    // "/api/agencies/:path*",
-    "/api/bookings/:path*",
-    "/api/brands/:path*",
-    "/api/carmodels/:path*",
-    "/api/cars/:path*",
-    "/api/catgories/:path*",
-  ],
+  matcher: ["/api/bookings/:path*"],
 };
